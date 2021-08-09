a72126fe4802d6f734f8d3d18f3752ecd2fe87e6
ltaylorbrown <lauren2.brown.lb@googlemail.com>
2021-08-09 16:46:59 +0100

    bookmarks

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ class BookmarkManager < Sinatra::Base
   get '/' do
     'Bookmark Manager'
   end
+  
+  get '/bookmarks' do
+    erb :bookmarks
+  end 
+
 
   run! if app_file == $0
 end
--- spec/features/viewing_bookmark_spec.rb
+++ spec/features/viewing_bookmark_spec.rb
@@ -3,4 +3,11 @@ feature 'Viewing bookmarks' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
   end
-end
+end 
+
+feature 'viewing bookmarks' do 
+  scenario 'visit /bookmarks' do 
+    visit ('/bookmarks')
+    expect(page).to have_content "Hello"
+  end
+end 
